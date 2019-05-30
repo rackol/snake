fe15f124733991d07fbbf004282691867264aeac
Rachel Pierce <rachelpierce@students.berkeley.net>
2019-05-30 13:22:34 -0700

    5/30

--- snake.js
+++ snake.js
@@ -1,17 +1,31 @@
-var snake;
+var s;
 var scl=20;
+var food;
 
 function setup(){
     createCanvas(600,600);
-    snake=new snake();
+    s=new Snake();
     frameRate(10);
+    pickLocation();
+}
 
+function pickLocation(){
+    var cols=floor(width/scl);
+    var rows=floor(height/scl);
+    food= createVector(floor(random(cols)),floor(random(rows)));
+    food.mult(scl);
 }
 
 function draw(){
     background (51);
-    this.update();
-    this.show();
+    s.update();
+    s.show();
+    if(s.eat(food)){
+        pickLocation();
+    }
+
+    fill(225,0,100);
+    rect(food.x,food.y,scl,scl);
 }
 
 function keyPressed(){
@@ -26,25 +40,48 @@ function keyPressed(){
     }
 }
 
-function snake(){
+function Snake(){
     this.x=0;
     this.y=0;
     this.xspeed=1;
     this.yspeed=0;
+    this.total=0;
+    this.tail=[];
 
     this.dir=function(x,y){
         this.xspeed=x;
         this.yspeed=y;
     }
     this.update=function(){
+        if(this.total==this.tail.length){
+            for(var i=0;i<this.tail.length-1;i++){
+                this.tail[i]=this.tail[i+1];
+            }
+            this.tail[this.total-1]=createVector(this.x,this.y);
+        }
+
         this.x+=this.xspeed*scl;
         this.y+=this.yspeed*scl;
         this.x=constrain(this.x,0,width-scl);
         this.y=constrain(this.y,0,height-scl);
+
     }
     this.show=function(){
         fill(255);
+        for(var i=0;i,this.tail.length-1;i++){
+            rect(this.tail[i].x,this.tail[i].y,scl,scl);
+        }
         rect(this.x,this.y,scl,scl);
+
+    }
+    this.eat=function(pos){
+        var d=dist(this.x,this.y,pos.x,pos.y);
+        if(d<1){
+            this.total++;
+            return true;
+        }else{
+            return false;
+        }
     }
 }
 
