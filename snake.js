2358b9f62486fe28928df49518d93297ef0498d2
Rachel Pierce <rachelpierce@students.berkeley.net>
2019-06-04 13:22:44 -0700

    6/4

--- snake.js
+++ snake.js
@@ -1,94 +1,115 @@
-var s;
-var scl=20;
+var high =0;
+var snake;
+var scl = 20;
 var food;
+var t = mousePressed();
 
-function setup(){
-    createCanvas(600,600);
-    s=new Snake();
+
+function setup() {
+    createCanvas(600, 600);
+    snake = new Snake();
     frameRate(10);
     pickLocation();
 }
 
-function pickLocation(){
-    var cols=floor(width/scl);
-    var rows=floor(height/scl);
-    food= createVector(floor(random(cols)),floor(random(rows)));
-    food.mult(scl);
-}
 
-function draw(){
-    background (51);
-    s.update();
-    s.show();
-    if(s.eat(food)){
-        pickLocation();
-    }
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+    this.total = 0;
+    this.tail = [];
 
-    fill(225,0,100);
-    rect(food.x,food.y,scl,scl);
-}
-
-function keyPressed(){
-    if(keyCode==UP_ARROW){
-        s.dir(0,-1);
-    }if(keyCode==DOWN_ARROW){
-        s.dir(0,1);
-    }if(keyCode==RIGHT_ARROW){
-        s.dir(1,0);
-    }if(keyCode==LEFT_ARROW){
-        s.dir(-1,0);
-    }
-}
-
-function Snake(){
-    this.x=0;
-    this.y=0;
-    this.xspeed=1;
-    this.yspeed=0;
-    this.total=0;
-    this.tail=[];
-
-    this.dir=function(x,y){
-        this.xspeed=x;
-        this.yspeed=y;
-    }
-    this.update=function(){
-        if(this.total==this.tail.length){
-            for(var i=0;i<this.tail.length-1;i++){
-                this.tail[i]=this.tail[i+1];
-            }
-            this.tail[this.total-1]=createVector(this.x,this.y);
-        }
-
-        this.x+=this.xspeed*scl;
-        this.y+=this.yspeed*scl;
-        this.x=constrain(this.x,0,width-scl);
-        this.y=constrain(this.y,0,height-scl);
-
-    }
-    this.show=function(){
-        fill(255);
-        for(var i=0;i,this.tail.length-1;i++){
-            rect(this.tail[i].x,this.tail[i].y,scl,scl);
-        }
-        rect(this.x,this.y,scl,scl);
-
-    }
-    this.eat=function(pos){
-        var d=dist(this.x,this.y,pos.x,pos.y);
-        if(d<1){
+    this.eat = function (pos) {
+        var d = dist(this.x, this.y, pos.x, pos.y);
+        if (d < 1) {
             this.total++;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
+
+    this.dir = function (x, y) {
+        this.xspeed = x;
+        this.yspeed = y;
+    }
+
+    this.death = function () {
+        for (var i = 0; i < this.tail.length; i++) {
+            var pos = this.tail[i];
+            var d = dist(this.x, this.y, pos.x, pos.y);
+            if (d < 1) {
+                console.log('starting over');
+                this.total = 0;
+                this.tail = [];
+            }
+        }
+    }
+
+    this.update = function () {
+        for (var i = 0; i < this.tail.length - 1; i++) {
+            this.tail[i] = this.tail[i + 1];
+        }
+        if (this.total >= 1) {
+            this.tail[this.total - 1] = createVector(this.x, this.y);
+        }
+        document.getElementById("total").innerHTML="Current Total:" + snake.total;
+        document.getElementById("high").innerHTML="High Score: " + high;
+        if(high<=snake.total){
+            high = snake.total;
+        document.getElementById("high").innerHTML="High Score: " + high;
+        }
+        this.x = this.x + this.xspeed * scl;
+        this.y = this.y + this.yspeed * scl;
+
+        this.x = constrain(this.x, 0, width - scl);
+        this.y = constrain(this.y, 0, height - scl);
+    }
+
+    this.show = function () {
+        fill(255);
+        for (var i = 0; i < this.tail.length; i++) {
+            rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        }
+        rect(this.x, this.y, scl, scl);
+
+    }
 }
 
-function rect(x,y){
-    var c=document.getElementById()
+
+function pickLocation() {
+    var cols = floor(width/scl);
+    var rows = floor(height/scl);
+    food = createVector(floor(random(cols)), floor(random(rows)));
+    food.mult(scl);
 }
 
-//background-image: url("image/537f897f97cc5.gif");
+function mousePressed() {
+    snake.total++;
+}
+function draw() {
+    background(10);
 
+    if (snake.eat(food)) {
+        pickLocation();
+    }
+    snake.death();
+    snake.update();
+    snake.show();
 
+    fill(175,100,220);
+    rect(food.x, food.y, scl, scl);
+}
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        snake.dir(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        snake.dir(0, 1);
+    } else if (keyCode === RIGHT_ARROW) {
+        snake.dir(1, 0);
+    } else if (keyCode === LEFT_ARROW) {
+        snake.dir(-1, 0);
+    }
+}
