91818936d44f4010a2c78f6ca3d2d263b6e9fc6e
Rachel Pierce <rachelpierce@students.berkeley.net>
2019-06-05 13:22:40 -0700

    6/5

--- snake.js
+++ snake.js
@@ -1,105 +1,103 @@
-var high =0;
+var high=0;
 var snake;
-var scl = 20;
+var scl=20;
 var food;
-var t = mousePressed();
+var t=mousePressed();
 
 
-function setup() {
-    createCanvas(600, 600);
-    snake = new Snake();
+
+function setup(){
+    createCanvas(600,600);
+    snake=new Snake();
     frameRate(10);
     pickLocation();
 }
 
+function Snake(){
+    this.x=0;
+    this.y=0;
+    this.xspeed=1;
+    this.yspeed=0;
+    this.total=0;
+    this.tail=[];
 
-function Snake() {
-    this.x = 0;
-    this.y = 0;
-    this.xspeed = 1;
-    this.yspeed = 0;
-    this.total = 0;
-    this.tail = [];
-
-    this.eat = function (pos) {
-        var d = dist(this.x, this.y, pos.x, pos.y);
-        if (d < 1) {
+    this.eat=function(pos){
+        var d=dist(this.x,this.y,pos.x,pos.y);
+        if(d<1){
             this.total++;
             return true;
-        } else {
+        }else{
             return false;
         }
     }
 
-    this.dir = function (x, y) {
-        this.xspeed = x;
-        this.yspeed = y;
+    this.dir=function(x, y){
+        this.xspeed=x;
+        this.yspeed=y;
     }
 
-    this.death = function () {
-        for (var i = 0; i < this.tail.length; i++) {
-            var pos = this.tail[i];
-            var d = dist(this.x, this.y, pos.x, pos.y);
-            if (d < 1) {
+    this.death=function(){
+        for(var i=0;i<this.tail.length;i++) {
+            var pos=this.tail[i];
+            var d=dist(this.x,this.y,pos.x,pos.y);
+            if (d<1){
                 console.log('starting over');
-                this.total = 0;
-                this.tail = [];
+                this.total=0;
+                this.tail=[];
             }
         }
     }
 
-    this.update = function () {
-        for (var i = 0; i < this.tail.length - 1; i++) {
-            this.tail[i] = this.tail[i + 1];
+    this.update=function(){
+        for(var i=0;i<this.tail.length-1;i++){
+            this.tail[i]=this.tail[i+1];
         }
-        if (this.total >= 1) {
-            this.tail[this.total - 1] = createVector(this.x, this.y);
+        if(this.total>=1){
+            this.tail[this.total-1]=createVector(this.x,this.y);
         }
-        document.getElementById("total").innerHTML="Current Total:" + snake.total;
-        document.getElementById("high").innerHTML="High Score: " + high;
+        document.getElementById("total").innerHTML="Current Score:"+snake.total;
+        document.getElementById("high").innerHTML="High Score: "+high;
         if(high<=snake.total){
-            high = snake.total;
-        document.getElementById("high").innerHTML="High Score: " + high;
+            high=snake.total;
+        document.getElementById("high").innerHTML="High Score: "+high;
         }
-        this.x = this.x + this.xspeed * scl;
-        this.y = this.y + this.yspeed * scl;
+        this.x=this.x+this.xspeed*scl;
+        this.y=this.y+this.yspeed*scl;
 
-        this.x = constrain(this.x, 0, width - scl);
-        this.y = constrain(this.y, 0, height - scl);
+        this.x=constrain(this.x,0,width-scl);
+        this.y=constrain(this.y,0,height-scl);
     }
 
-    this.show = function () {
+    this.show=function(){
         fill(255);
-        for (var i = 0; i < this.tail.length; i++) {
-            rect(this.tail[i].x, this.tail[i].y, scl, scl);
+        for (var i=0;i<this.tail.length;i++){
+            rect(this.tail[i].x,this.tail[i].y,scl,scl);
         }
-        rect(this.x, this.y, scl, scl);
-
+        rect(this.x,this.y,scl,scl);
     }
 }
 
 
-function pickLocation() {
-    var cols = floor(width/scl);
-    var rows = floor(height/scl);
-    food = createVector(floor(random(cols)), floor(random(rows)));
+function pickLocation(){
+    var cols=floor(width/scl);
+    var rows=floor(height/scl);
+    food=createVector(floor(random(cols)),floor(random(rows)));
     food.mult(scl);
 }
 
-function mousePressed() {
+function mousePressed(){
     snake.total++;
 }
-function draw() {
+function draw(){
     background(10);
 
-    if (snake.eat(food)) {
+    if (snake.eat(food)){
         pickLocation();
     }
     snake.death();
     snake.update();
     snake.show();
-
-    fill(175,100,220);
+    fill(139,0,139);
     rect(food.x, food.y, scl, scl);
 }
 function keyPressed() {
